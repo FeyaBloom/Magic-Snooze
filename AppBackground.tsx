7a687cbd8f2ceaf44cc907c09026a50a9a91a806
FeyaBloom <feya.bloom.design@gmail.com>
2025-07-26 12:19:42 +0200

    Updated AppBackground.tsx

---
+++ AppBackground.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { useTheme } from '@/components/ThemeProvider';
+import { FloatingCloud, GentleStars } from '@/components/MagicFX';
+
+export default function AppBackground() {
+  const { theme } = useTheme();
+
+  return (
+    <View style={StyleSheet.absoluteFill} pointerEvents="none">
+      {theme === 'daydream' && <FloatingCloud />}
+      {theme === 'nightforest' && <GentleStars />}
+    </View>
+  );
+}
