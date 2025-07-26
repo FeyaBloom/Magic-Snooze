9dbbb02691b855a4a78b73523a34c47047f91754
FeyaBloom <feya.bloom.design@gmail.com>
2025-07-26 12:37:21 +0200

    Fix AppBackground component import

--- components/AppBackground.tsx
+++ components/AppBackground.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { useTheme } from '@/components/ThemeProvider';
-import { FloatingCloud, GentleStars } from '@/components/MagicalFeatures';
+import { FloatingBackground } from '@/components/MagicalFeatures';
 
 export default function AppBackground() {
   const { theme } = useTheme();
 
   return (
     <View style={StyleSheet.absoluteFill} pointerEvents="none">
-      {theme === 'daydream' && <FloatingCloud />}
-      {theme === 'nightforest' && <GentleStars />}
+      <FloatingBackground />
     </View>
   );
 }
