5ba88e2cde76a4f669fce2880d6a216075287653
FeyaBloom <feya.bloom.design@gmail.com>
2025-07-26 16:29:59 +0200

    Updated MagicalFeatures.tsx

--- components/MagicalFeatures.tsx
+++ components/MagicalFeatures.tsx
@@ -203,7 +203,7 @@ const styles = StyleSheet.create({
   },
   sparkle: { position: 'absolute', zIndex: 10 },
   tinyVictoryContainer: {
-    backgroundColor: colors.surface,
+    backgroundColor: `#ffffff`,
     borderRadius: 20,
     padding: 20,
     margin: 20,
