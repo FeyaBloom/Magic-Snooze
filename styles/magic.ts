791343258303ac1b59b2c2726e7160721ddb9c86
FeyaBloom <feya.bloom.design@gmail.com>
2025-07-28 14:42:21 +0200

    Updated magic.ts

--- styles/magic.ts
+++ styles/magic.ts
@@ -55,7 +55,7 @@ export const createMagicStyles = (colors: any) => StyleSheet.create({
   },
   victoryButton: {
     width: '48%',
-    backgroundColor: colors.Primary,
+    backgroundColor: colors.primary,
     borderRadius: 12,
     padding: 12,
     marginBottom: 8,
