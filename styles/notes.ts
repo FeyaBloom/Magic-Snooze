7d41e45a4120c1f13022c058b3bf066a80599e95
FeyaBloom <feya.bloom.design@gmail.com>
2025-07-28 16:53:11 +0200

    Updated notes.ts

--- styles/notes.ts
+++ styles/notes.ts
@@ -160,6 +160,7 @@ export const createNotesStyles = (colors: any) => StyleSheet.create({
     padding: 24,
     width: '100%',
     maxWidth: 400,
+    height: 200,
     maxHeight: '80%',
   },
   modalTitle: {
