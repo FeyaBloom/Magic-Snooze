c2294bed3b22ea3d0f26840333e388f553724dc1
FeyaBloom <feya.bloom.design@gmail.com>
2025-07-28 14:53:00 +0200

    Updated magic.ts

--- styles/magic.ts
+++ styles/magic.ts
@@ -18,7 +18,7 @@ export const createMagicStyles = (colors: any) => StyleSheet.create({
   },
   magicalCheckboxDisabled: {
     backgroundColor: colors.disabled,
-    borderColor: colors.secondary + `10`,
+    borderColor: colors.secondary + `20`,
   },
   sparkle: { position: 'absolute', zIndex: 10 },
 
