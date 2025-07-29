da2da8dc4e298bb041c9d6b1ba84e797d77a74d4
FeyaBloom <feya.bloom.design@gmail.com>
2025-07-29 15:08:47 +0200

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 import { ThemeProvider } from '@/components/ThemeProvider';
+import {FloatingBackground} from "@/components/MagicalFeatures"
 
 
 import { useFonts } from 'expo-font';
