2c5f052087565f3e55a565bd24e8d7b6d549b4c6
FeyaBloom <feya.bloom.design@gmail.com>
2025-07-30 14:08:44 +0200

    Updated MagicalFeatures.tsx

--- components/MagicalFeatures.tsx
+++ components/MagicalFeatures.tsx
@@ -20,10 +20,10 @@ const { width: screenWidth } = Dimensions.get('window');
 // ---- FLOATING BACKGROUNDS ----
 
 
-export const FloatingCloud = () => ( <LottieView source={FloatingCloudJSON} autoPlay loop style={StyleSheet.absoluteFillObject,  {zIndex: 0}}
+export const FloatingCloud = () => ( <LottieView source={FloatingCloudJSON} autoPlay loop style={StyleSheet.absoluteFillObject,  {zIndex: -1}}
  /> );
 
-export const GentleStars = () => ( <LottieView source={GentleStarsJSON} autoPlay loop style={[StyleSheet.absoluteFillObject, {zIndex: 0}]} /> );
+export const GentleStars = () => ( <LottieView source={GentleStarsJSON} autoPlay loop style={[StyleSheet.absoluteFillObject, {zIndex: -1}]} /> );
 
 export const FloatingBackground: React.FC = () => {
   const { currentTheme } = useTheme();
