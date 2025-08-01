9ab7865cd92732d4f26ba7e0063bb7ac07b7d2ed
FeyaBloom <feya.bloom.design@gmail.com>
2025-08-01 03:08:51 +0200

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -14,7 +14,7 @@ export default function TabLayout() {
           backgroundColor: colors.surface,
           borderTopWidth: 0,
           borderBottomWidth: 1,
-          borderBottomColor: colors.secondary + '10',
+          borderBottomColor: colors.secondary + '50',
           elevation: 8,
           shadowColor: '#000',
           shadowOffset: { width: 0, height: 2 },
