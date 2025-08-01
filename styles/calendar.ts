0a4138deeb67ea06a87dd3f563777bd1dd03a7ba
FeyaBloom <feya.bloom.design@gmail.com>
2025-08-02 00:48:19 +0200

    Updated calendar.ts

--- styles/calendar.ts
+++ styles/calendar.ts
@@ -74,7 +74,7 @@ disabledDayText: {
     marginBottom: 20,
   },
   navButton: {
-    padding: 8,
+    padding: 18,
     borderRadius: 20,
     backgroundColor: colors.secondary + '20',
     width: 40,
