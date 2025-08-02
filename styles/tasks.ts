66a99d0cad1973daa2a35f3ecd15c0af0bf39e3a
FeyaBloom <feya.bloom.design@gmail.com>
2025-08-02 02:10:19 +0200

    Updated tasks.ts

--- styles/tasks.ts
+++ styles/tasks.ts
@@ -20,7 +20,7 @@ export const createTasksStyles = (colors: any) => StyleSheet.create({
   },
   title: {
     fontSize: 24,
-    fontWeight: '1000',
+  //  fontWeight: '1000',
     color: colors.text,
     marginBottom: 8,
     textAlign: 'center',
