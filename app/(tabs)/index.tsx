9add0c610e778b69ead23cde480153fbee593e1f
FeyaBloom <feya.bloom.design@gmail.com>
2025-08-03 16:02:09 +0200

    Updated index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,6 +17,7 @@ import { MagicalCheckbox, TinyVictoryTracker, SurprisePrompt, } from '@/componen
 import { useTheme } from '@/components/ThemeProvider';
 import { createTodayStyles } from '@/styles/today';
 import {FloatingBackground} from "@/components/MagicalFeatures";
+import {ConfirmDialog} from "@/components/confirmDialog"
 
 
 interface RoutineStep {
