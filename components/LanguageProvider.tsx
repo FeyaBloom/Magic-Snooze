b2a0addcde06b878a85b79f7695db17ab26896a7
FeyaBloom <feya.bloom.design@gmail.com>
2025-08-04 18:49:42 +0200

    Updated LanguageProvider.tsx

--- components/LanguageProvider.tsx
+++ components/LanguageProvider.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { translations } from '@/constants/translations';
+import { translations } from '@/components/translations';
 
 const LanguageContext = createContext();
 
