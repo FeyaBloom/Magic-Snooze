8631962cdd28de7b0d2f8081f36fbe6995671f00
FeyaBloom <feya.bloom.design@gmail.com>
2025-08-04 21:40:57 +0200

    Updated LanguageProvider1.tsx

--- components/LanguageProvider1.tsx
+++ components/LanguageProvider1.tsx
@@ -1,5 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import i18n from '@/i18n';
 
 export type LanguageCode = 'en' | 'ru' | 'es' | 'fr' | 'de';
 
