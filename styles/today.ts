2015569496fef8e6eb605a58f6f165a89901e369
FeyaBloom <feya.bloom.design@gmail.com>
2025-08-05 01:11:22 +0200

    Updated today.ts

--- styles/today.ts
+++ styles/today.ts
@@ -185,6 +185,7 @@ export const createTodayStyles = (colors: any) => StyleSheet.create({
     marginHorizontal: 4,
     backgroundColor: colors.surface,
     height: 48,
+    marginRight: 14,
   },
   themeButtonText: {
     fontSize: 14,
