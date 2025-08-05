96c364a100c62c0d9f14602bf8253a50a58edc7b
FeyaBloom <feya.bloom.design@gmail.com>
2025-08-05 13:33:43 +0200

    Updated customCalendar.tsx

--- components/customCalendar.tsx
+++ components/customCalendar.tsx
@@ -7,7 +7,7 @@ import {
 import { ChevronLeft, ChevronRight } from 'lucide-react-native';
 import { useTheme } from '@/components/ThemeProvider';
 import { createCalendarStyles, dayWidth } from '@/styles/calendar';
-
+import i18n from '@/i18n';
 interface CalendarProps {
   // Для выбора даты (модалка)
   selectedDate?: Date;
