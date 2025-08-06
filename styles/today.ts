318e206911c909288484bc0cf1f57ac82d8d58d8
FeyaBloom <feya.bloom.design@gmail.com>
2025-08-07 01:22:44 +0200

    Updated today.ts

--- styles/today.ts
+++ styles/today.ts
@@ -186,7 +186,7 @@ export const createTodayStyles = (colors: any) => StyleSheet.create({
     marginHorizontal: 16,
   },
   themeButtonText: {
-    fontSize: 14,
+    fontSize: 12,
     fontWeight: '600',
     color: colors.text,
     fontFamily: 'ComicNeue-Regular',
