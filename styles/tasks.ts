69cef619466800a8cc3a60a32da58e9203ac925c
FeyaBloom <feya.bloom.design@gmail.com>
2025-08-07 02:06:33 +0200

    Updated tasks.ts

--- styles/tasks.ts
+++ styles/tasks.ts
@@ -38,7 +38,7 @@ export const createTasksStyles = (colors: any) => StyleSheet.create({
     backgroundColor: colors.primary,
     marginHorizontal: 20,
     marginBottom: 20,
-    paddingVertical: 10,
+    paddingVertical: 8,
     borderRadius: 16,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
