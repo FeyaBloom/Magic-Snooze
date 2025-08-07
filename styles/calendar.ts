8bc631640ef362a1c0f47a1346c518cf4dd32f9c
FeyaBloom <feya.bloom.design@gmail.com>
2025-08-07 02:13:33 +0200

    Updated calendar.ts

--- styles/calendar.ts
+++ styles/calendar.ts
@@ -94,10 +94,8 @@ disabledDayText: {
     flexDirection: 'row',
     marginBottom: 10,
     alignItems: 'center',
-    marginLeft: 20,
-    marginRight: 20,
-  },
   
+  },
   weekDayText: {
     fontSize: 14,
     fontWeight: '600',
