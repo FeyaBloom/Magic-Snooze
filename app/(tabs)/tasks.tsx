7f4c0796409aeefc249d0face4df7d875cca39b9
FeyaBloom <feya.bloom.design@gmail.com>
2025-08-07 22:54:56 +0200

    Updated tasks.tsx

--- app/(tabs)/tasks.tsx
+++ app/(tabs)/tasks.tsx
@@ -33,8 +33,10 @@ interface Task {
 export default function TasksTab() {
   const currentLanguageCode = i18n.language;
   const [languageModalVisible, setLanguageModalVisible] = useState(false);
-  const { colors } = useTheme();
-  const gradient = getTabGradient(route.name);
+ const route = useRoute();
+const { colors, getTabGradient, currentTheme } = useTheme();
+const gradient = getTabGradient(route.name);
+
   const styles = createTasksStyles(colors);
   const [tasks, setTasks] = useState<Task[]>([]);
   const [showAddModal, setShowAddModal] = useState(false);
