e31fe79539ed16d79804d037d238fe2ab5e188a8
FeyaBloom <feya.bloom.design@gmail.com>
2025-08-07 04:45:11 +0200

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout() {
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
             <Stack.Screen name="+not-found" />
           </Stack>
-          <StatusBar style="auto" />
+          <StatusBar hidden />
         </ThemeProvider>
       </I18nextProvider>
     </>
