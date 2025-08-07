f170016410ca2c470f974f6bc66b922849cf11ec
FeyaBloom <feya.bloom.design@gmail.com>
2025-08-07 20:17:41 +0200

    Updated index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -23,7 +23,7 @@ import { ConfirmDialog } from "@/components/confirmDialog";
 import { useRouter } from 'expo-router';
 
 const { t } = i18n;
-
+const gradient = colors.tabGradients?.today;
 interface RoutineStep {
   id: string;
   text: string;
