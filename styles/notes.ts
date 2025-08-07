f655a94c18caaa63cce3c2ac6143d228a0b86c55
FeyaBloom <feya.bloom.design@gmail.com>
2025-08-07 03:16:58 +0200

    Updated notes.ts

--- styles/notes.ts
+++ styles/notes.ts
@@ -118,7 +118,7 @@ export const createNotesStyles = (colors: any) => StyleSheet.create({
     fontFamily: 'ComicNeue-Regular',
   },
   notePreview: {
-    fontSize: 13,
+    fontSize: 15,
     color: colors.textSecondary,
     lineHeight: 20,
     marginBottom: 12,
