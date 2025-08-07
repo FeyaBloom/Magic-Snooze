09175d22ec7a05d1d9623397e8236fc789b28d8d
FeyaBloom <feya.bloom.design@gmail.com>
2025-08-07 04:22:23 +0200

    Updated settings.tsx

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -75,7 +75,8 @@ export default function SettingsTab() {
               <View>
                 <Text style={styles.label}>{t('settings.language.title')}</Text>
                 <Text style={styles.description}>
-                  {t('settings.language.currently')}: {currentLanguageCode.toUpperCase()} //флажок
+                 
+                  {t('settings.language.currently')}: {t(`languages.${currentLanguageCode}.flag`)} {t(`languages.${currentLanguageCode}.native`)}
                 </Text>
               </View>
               <Languages color={colors.textSecondary} size={20} />
