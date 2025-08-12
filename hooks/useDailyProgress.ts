d674d6cc2d050c0645d20c269cd31006f37a262b
FeyaBloom <feya.bloom.design@gmail.com>
2025-08-12 19:43:02 +0200

    Updated useDailyProgress.ts

--- hooks/useDailyProgress.ts
+++ hooks/useDailyProgress.ts
@@ -12,7 +12,7 @@ export interface DailyProgress {
   snoozed: boolean;
 }
 
-const getLocalDateString = (date: Date) => {
+const getLocalDateString = (date: Date = new Date()) => {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
